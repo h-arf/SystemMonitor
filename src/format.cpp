e70f4d370bbccbfb5cb3ec7a3135eb631acbb2e5
h-arieff <80794782+h-arieff@users.noreply.github.com>
2021-06-08 18:13:48 -0400

    added uptime debug_print

--- src/format.cpp
+++ src/format.cpp
@@ -12,15 +12,13 @@ string Format::ElapsedTime(long s) {
 #ifdef DEBUG_PRINT
     std::cerr<<"star s is: "<<s<<std::endl;
 #endif
-    long m,h;
-    m=s/60;
-    s=s%60;
-    h=m/60;
-    m=m%60;
-    h=h%24;
+    double m,h;
+    h=s/3600;
+    m=(h-(long)h)*60;
+    s=(m-(long)m)*60;
     string r;
 #ifdef DEBUG_PRINT
     std::cerr<<h<<":"<<m<<":"<<s<<std::endl;
 #endif
-    return to_string(h)+":"+to_string(m)+":"+to_string(s);
+    return to_string((long)h)+":"+to_string((long)m)+":"+to_string(s);
 }
