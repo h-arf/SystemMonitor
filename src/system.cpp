9258fe6d50d4d253172af1134ce6179772e94f99
h-arieff <80794782+h-arieff@users.noreply.github.com>
2021-06-08 18:33:40 -0400

    added memutil

--- src/system.cpp
+++ src/system.cpp
@@ -37,7 +37,10 @@ std::string System::Kernel()
 }
 
 // TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+float System::MemoryUtilization()
+{ 
+    return LinuxParser::MemoryUtilization();
+}
 
 
 std::string System::OperatingSystem()
