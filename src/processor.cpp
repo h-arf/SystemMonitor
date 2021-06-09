5d4f9e77f7794e4428a3f80bcc87b4884bc3402a
h-arieff <80794782+h-arieff@users.noreply.github.com>
2021-06-09 18:00:10 -0400

    added cpu util

--- src/processor.cpp
+++ src/processor.cpp
@@ -11,12 +11,9 @@ float Processor::Utilization()
     std::vector<unsigned long> r=LinuxParser::CpuUtilization();
     unsigned long tt=std::accumulate(r.begin(), r.end(), 0ll);
     unsigned long idle=r[3];
-    std::cerr<<tt<<" "<<idle<<" "<<r[3]<<std::endl;
     unsigned long i_d=idle-prev_idle;
     unsigned long t_d=tt-prev_total;
-    std::cerr<<t_d<<" "<<i_d<<std::endl;
     prev_idle=idle;
     prev_total=tt;
-    std::cerr<<(float)(1.0-(float)(i_d/t_d))<<std::endl;
     return (1.0-(float)i_d/t_d);
 }
