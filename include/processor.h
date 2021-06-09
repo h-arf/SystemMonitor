c076e66c44ac7f781a4db583e501744c4204cd1f
h-arieff <80794782+h-arieff@users.noreply.github.com>
2021-06-09 17:23:16 -0400

    added cpu util

--- include/processor.h
+++ include/processor.h
@@ -7,17 +7,6 @@ class Processor {
 
   // TODO: Declare any necessary private members
  private:
-  unsigned long long PrevIdle{0};
-  unsigned long longIdle{0};
-
-  unsigned long longPrevNonIdle{0};
-  unsigned long longNonIdle{0};
-
-  unsigned long longPrevTotal{0};
-  unsigned long longTotal{0};
-
-  unsigned long longtotald{0};
-  unsigned long longidled{0};
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -7,27 +7,6 @@ float Processor::Utilization()
 {
     //from https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
     std::vector<unsigned long long> r=LinuxParser::CpuUtilization();
-    unsigned long long user{r[0]},nice{r[1]},system{r[2]},idle{r[3]},iowait{r[4]},irq{r[5]},softirq{r[6]},steal{r[7]},guest{r[8]},guest_nice{r[9]};
-    unsigned long long intusertime = usertime - guest;                             # As you see here, it subtracts guest from user time
-    unsigned long long intnicetime = nicetime - guest_nice;                         # and guest_nice from nice time
-// Fields existing on kernels >= 2.6
-// (and RHEL's patched kernel 2.4...)
-    unsigned long long int idlealltime = idletime + ioWait;  # ioWait is added in the idleTime
-    unsigned long long int systemalltime = systemtime + irq + softIrq;
-    unsigned long long int virtalltime = guest + guest_nice;
-    unsigned long long int totaltime = usertime + nicetime + systemalltime + idlealltime + steal + virtalltime;
-    PrevIdle = previdle + previowait
-    Idle = idle + iowait
-
-    PrevNonIdle = prevuser + prevnice + prevsystem + previrq + prevsoftirq + prevsteal
-    NonIdle = user + nice + system + irq + softirq + steal
-
-    PrevTotal = PrevIdle + PrevNonIdle
-    Total = Idle + NonIdle
-
-    totald = Total - PrevTotal
-    idled = Idle - PrevIdle
-
-    CPU_Percentage = (totald - idled)/totald;
-    return CPU_Percentage;
+    unsigned long long tt{(r[0])+(r[1])+(r[2])+(r[3])+(r[4])+(r[5])+(r[6])+(r[7])+(r[8])+(r[9])};
+    return tt/(long double)r[3];
 }
