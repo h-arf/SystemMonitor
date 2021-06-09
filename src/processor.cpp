dc235474ba3c4b836fd72a5830d443856a19cda1
h-arieff <80794782+h-arieff@users.noreply.github.com>
2021-06-09 17:12:10 -0400

    added cpu util

--- src/processor.cpp
+++ src/processor.cpp
@@ -9,12 +9,12 @@ float Processor::Utilization()
     std::vector<unsigned long long> r=LinuxParser::CpuUtilization();
     unsigned long long user{r[0]},nice{r[1]},system{r[2]},idle{r[3]},iowait{r[4]},irq{r[5]},softirq{r[6]},steal{r[7]},guest{r[8]},guest_nice{r[9]};
     unsigned long long intusertime = usertime - guest;                             # As you see here, it subtracts guest from user time
-    unsigned long long intnicetime = nicetime - guestnice;                         # and guest_nice from nice time
+    unsigned long long intnicetime = nicetime - guest_nice;                         # and guest_nice from nice time
 // Fields existing on kernels >= 2.6
 // (and RHEL's patched kernel 2.4...)
     unsigned long long int idlealltime = idletime + ioWait;  # ioWait is added in the idleTime
     unsigned long long int systemalltime = systemtime + irq + softIrq;
-    unsigned long long int virtalltime = guest + guestnice;
+    unsigned long long int virtalltime = guest + guest_nice;
     unsigned long long int totaltime = usertime + nicetime + systemalltime + idlealltime + steal + virtalltime;
     PrevIdle = previdle + previowait
     Idle = idle + iowait
