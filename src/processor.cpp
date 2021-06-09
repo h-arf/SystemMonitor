7e2074b7ea0bb598720321a567f217ef938e15ff
h-arieff <80794782+h-arieff@users.noreply.github.com>
2021-06-09 17:46:47 -0400

    added cpu util

--- src/processor.cpp
+++ src/processor.cpp
@@ -16,5 +16,6 @@ float Processor::Utilization()
     long double t_d=tt-prev_total;
     prev_idle=idle;
     prev_total=tt;
+    std::cerr<<(float)(1.0-(t_d/i_d))<<std::endl;
     return (float)(1.0-(t_d/i_d));
 }
