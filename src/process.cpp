df88f90a3a956a53228dc07e67938959323d6373
h-arieff <80794782+h-arieff@users.noreply.github.com>
2021-06-10 18:25:04 -0400

    fix

--- src/process.cpp
+++ src/process.cpp
@@ -27,7 +27,7 @@ float Process::CpuUtilization() const{
     //std::cerr<<utime<<" "<<stime<<" "<<cutime<<" "<<cstime<<" "<<starttime<<" "<<LinuxParser::UpTime()<<" "<<Hertz<<std::endl;
     unsigned long long total_time = utime + stime;// + cutime + cstime;
     float seconds = LinuxParser::UpTime() - (float)(starttime / Hertz);
-    return 10*(float)((total_time / Hertz) / seconds);
+    return 100*(float)((total_time / Hertz) / seconds);
 }
 
 // TODO: Return the command that generated this process
